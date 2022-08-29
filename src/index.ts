a3e3513263341df7a665d8f836c24a1c1a9f653e
UnknownPerson3420 <76407758+UnknownPerson3420@users.noreply.github.com>
2022-08-28 21:08:37 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@ app.get("/", async (req, res) => {
 app.get('/login', (req, res) => {
   res.sendFile('login.html');
 });
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
+});
 app.listen(port, () => {
   console.log(`Example app listening at https://usmc.up.railway.app/`);
 });
